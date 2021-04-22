d642d945ac50cef5edf85a81976b15a4f5ce7122
ritikadhawan <ritikadhawan28@gmail.com>
2021-04-22 13:26:34 +0530

    grid paths

--- Introductory Problems/gridPaths.cpp
+++ Introductory Problems/gridPaths.cpp
@@ -1,68 +1,52 @@
 #include<bits/stdc++.h>
 using namespace std;
-
-long long noOfPaths(string pathDes, vector<vector<bool>>&visited, int i, int j, int currP, vector<vector<vector<long long>>>&storage) {
-    if(i < 0 || i >= 7) {
-        return 0;
-    } 
-    if(j < 0 || j >= 7) {
-        return 0;
-    }
-
-    if(visited[i][j]) {
-        return 0;
-    }
-
-    if(i == 6 && j == 0 && currP == pathDes.size()) {
-        //destination
-        return 1;
-    }
-    if(currP == pathDes.size()) {
-        return 0;
-    }
-    if(i == 6 && j == 0) {
-        return 0;
-    }
-    // cout<<i<<" "<<j<<" "<<currP<<" "<<storage[i][j][currP]<<endl;
-    if(storage[i][j][currP] != -1) {
-        // cout<<i<<" "<<j<<" "<<currP<<endl;
-        return storage[i][j][currP];
-    }
-    visited[i][j] = true;
-    long long paths = 0;
+class Directions {
+    public:
+    bool up, down, left, right;
+};
+Directions possibleDirections(string&pathDes, vector<vector<bool>>&visited, int i, int j, int currP) {
+    Directions ans;
     if(pathDes[currP] == '?') {
-        paths += noOfPaths(pathDes, visited, i+1, j, currP+1, storage);
-        paths += noOfPaths(pathDes, visited, i-1, j, currP+1, storage);
-        paths += noOfPaths(pathDes, visited, i, j-1, currP+1, storage);
-        paths += noOfPaths(pathDes, visited, i, j+1, currP+1, storage);
+        ans.up = i - 1 >= 0 && !visited[i - 1][j];
+        ans.down = i + 1 < 7 && !visited[i + 1][j];
+        ans.left = j - 1 >= 0 && !visited[i][j - 1];
+        ans.right = j + 1 < 7 && !visited[i][j + 1]; 
     } else {
-        switch(pathDes[currP]) {
-            case 'D':
-                paths += noOfPaths(pathDes, visited, i+1, j, currP+1, storage);
-            break;
-            case 'U':
-                paths += noOfPaths(pathDes, visited, i-1, j, currP+1, storage);
-            break;
-            case 'L':
-                paths += noOfPaths(pathDes, visited, i, j-1, currP+1, storage);
-            break;
-            case 'R':
-                paths += noOfPaths(pathDes, visited, i, j+1, currP+1, storage);
-            break;
-        }
+        ans.up = pathDes[currP] == 'U' && i - 1 >= 0 && !visited[i - 1][j];
+        ans.down = pathDes[currP] == 'D' && i + 1 < 7 && !visited[i + 1][j]; 
+        ans.left = pathDes[currP] == 'L' && j - 1 >= 0 && !visited[i][j - 1];
+        ans.right = pathDes[currP] == 'R' && j + 1 < 7 && !visited[i][j + 1];
     }
+    return ans;
+}
+long long noOfPaths(string&pathDes, vector<vector<bool>>&visited, int i, int j, int currP) {
+    if(i == 6 && j == 0) {
+        return (currP == pathDes.length()) ? 1 : 0;
+    }
+ 
+    //pruning the search here as we need to visit each cell atleast once and if we need to choose between left and right OR up and down we can stop
+    Directions d = possibleDirections(pathDes, visited, i, j, currP);
+    if(pathDes[currP] == '?' && ((!d.up && !d.down && d.left && d.right) || (d.up && d.down && !d.left && !d.right))) {
+        return 0;
+    }
+ 
+    visited[i][j] = true;
+    long long ans = 0;
+ 
+    ans += d.up ? noOfPaths(pathDes, visited, i - 1, j, currP + 1) : 0;
+    ans += d.down ? noOfPaths(pathDes, visited, i + 1, j, currP + 1) : 0;
+    ans += d.left ? noOfPaths(pathDes, visited, i, j - 1, currP + 1) : 0;
+    ans += d.right ? noOfPaths(pathDes, visited, i, j + 1, currP + 1) : 0;
+    
     visited[i][j] = false;
-    storage[i][j][currP] = paths;
-    // cout<<i<<" "<<j<<" "<<currP<<" "<<storage[i][j][currP]<<endl;
-    return paths;
+    return ans;
 }
 int main() {
     string pathDes;
     cin>>pathDes;
-
-    vector<vector<vector<long long>>> storage(8, vector<vector<long long>>(8, vector<long long>(50, -1)));
-    vector<vector<bool>> visited(8, vector<bool>(8, false));
-
-    cout<<noOfPaths(pathDes, visited, 0, 0, 0, storage);
+ 
+    vector<vector<bool>> visited(7, vector<bool>(7, false));
+ 
+    cout<<noOfPaths(pathDes, visited, 0, 0, 0);
     return 0;
 }
